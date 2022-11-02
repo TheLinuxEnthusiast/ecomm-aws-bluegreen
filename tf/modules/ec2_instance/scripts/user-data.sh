e860e78b7b270f4239cfdd2d06313069d239aac9
terraform-admin-at-275562404519 <darrenfoley015@gmail.com>
2022-11-02 19:15:45 +0000

    Added restart policy to docker container

--- tf/modules/ec2_instance/scripts/user-data.sh
+++ tf/modules/ec2_instance/scripts/user-data.sh
@@ -3,4 +3,4 @@
 #export MARIADB_ROOT_PASSWORD="12345"
 #sudo sh -c "echo export MARIADB_ROOT_PASSWORD='12345' >> /etc/profile.d/maria.sh"
 cd /home/ec2-user/ecomm-lamp-app/
-sudo docker-compose up --detach
+docker-compose up --detach
