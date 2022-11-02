9249ec453519fb4771ad714fc423db5627234a11
terraform-admin-at-275562404519 <darrenfoley015@gmail.com>
2022-11-02 23:36:10 +0000

    Updated user-data script with absolute paths

--- tf/modules/ec2_instance/scripts/user-data.sh
+++ tf/modules/ec2_instance/scripts/user-data.sh
@@ -3,4 +3,4 @@
 #export MARIADB_ROOT_PASSWORD="12345"
 #sudo sh -c "echo export MARIADB_ROOT_PASSWORD='12345' >> /etc/profile.d/maria.sh"
 cd /home/ubuntu/ecomm-lamp-app/
-nohup docker-compose up --detach &
+nohup /usr/local/bin/docker-compose -f /home/ubuntu/ecomm-lamp-app/docker-compose.yml up --detach &
