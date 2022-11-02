bb5b15a1e3bf447fe094fa77b4a4699ff95e01a1
terraform-admin-at-275562404519 <darrenfoley015@gmail.com>
2022-11-02 23:08:33 +0000

    run script in background

--- tf/modules/ec2_instance/scripts/user-data.sh
+++ tf/modules/ec2_instance/scripts/user-data.sh
@@ -3,4 +3,4 @@
 #export MARIADB_ROOT_PASSWORD="12345"
 #sudo sh -c "echo export MARIADB_ROOT_PASSWORD='12345' >> /etc/profile.d/maria.sh"
 cd /home/ubuntu/ecomm-lamp-app/
-nohup docker-compose up --detach
+nohup docker-compose up --detach &
