37241e521bd86788f44d367d362feb8fd06e8c21
Darren Foley <darrenfoley015@gmail.com>
2023-01-04 22:16:50 +0000

    Added new new modules for ECS Blue/Green Deployment

---
+++ tf/modules/blue/main.tf
@@ -0,0 +1,35 @@
+
+# resource "aws_alb_target_group" "ecomm_app_group_blue" {
+#   name        = "terraform-alb-target-blue"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = var.ecomm_vpc_id
+#   target_type = "ip"
+#   tags = {
+#     Environment = "${terraform.workspace}"
+#     Name        = "${var.prefix}-blue-${var.suffix}"
+#   }
+# }
+
+resource "aws_ecs_service" "ecomm_service_blue" {
+  name             = "ecomm-lamp-app-${var.type}"
+  cluster          = var.ecs_cluster_id     #aws_ecs_cluster.ecomm_cluster.id
+  task_definition  = var.task_definition_id #aws_ecs_task_definition.ecomm_app_task.id
+  desired_count    = 2
+  launch_type      = "FARGATE"
+  platform_version = "LATEST"
+
+  network_configuration {
+    security_groups = [var.security_group_id] #[aws_security_group.ecomm_sg.id]
+    subnets         = var.private_subnets
+  }
+
+  load_balancer {
+    target_group_arn = var.ecomm_app_group_blue.arn
+    container_name   = "ecomm-lamp-app"
+    container_port   = 80
+  }
+
+  depends_on = [var.ecomm_alb_listener]
+
+}
---
+++ tf/modules/blue/output.tf
@@ -0,0 +1,4 @@
+
+#output "alb_target_group_green_arn" {
+#    value = aws_alb_target_group.ecomm_app_group_blue.arn
+#}
